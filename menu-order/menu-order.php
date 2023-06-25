2c681a51d9fe1af61cc76a3f14b0a319cb4336a4
DotifyBIZ <134603838+DotifyBIZ@users.noreply.github.com>
2023-06-25 22:58:02 +0200

    Added "Enable Menu Order for Posts"
    Uploaded the first (1.0) version of "Enable Menu Order for Posts" plugin for WordPress.

---
+++ menu-order/menu-order.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Enable Menu Order for Posts
+Plugin URI: https://github.com/DotifyBIZ/wordpress-plugins/menu-order
+Description: Adds menu order and template to post types.
+Version: 1.0
+Author: Dotify
+Author URI: https://dotify.biz
+*/
+
+add_action( 'init', 'wpse31629_init' );
+function wpse31629_init()
+{
+	add_post_type_support( 'post', 'page-attributes' );
+}
+?>
